4eec4a7b7a6fee4833f87ec0b8b1b62c619f3597
Marius Vuscan <marius.vuscan@gmail.com>
2018-11-26 14:09:06 +0200

    Initial commit

---
+++ Implementare algoritmii criptare .net/Implementare algoritmii criptare .net/EncryptionAlgorithm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Implementare_algoritmii_criptare.net
+{
+    public enum Algorithm
+    {
+        Rijndael,
+        Aes,
+        DES,
+        TripleDES,
+        RC2
+    }
+
+    public static class EncryptionAlgorithm
+    {
+        public static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV, CipherMode cm,PaddingMode pm, Algorithm a,ref string SizeString)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            using (SymmetricAlgorithm sa = SymmetricAlgorithm.Create(a.ToString()))
+            {
+                sa.Key = Key;
+                sa.IV = IV;
+                sa.Mode = cm;
+                sa.Padding = pm;
+
+                //key size & block size
+                SizeString = "";
+                KeySizes[] ks = sa.LegalKeySizes;
+                SizeString += "No empty fields!\n";
+                SizeString += a.ToString() + " key and block sizes are: ";
+                foreach (KeySizes k in ks)
+                {
+                    SizeString+="\n\nLegal min key size = " + k.MinSize+" bits";
+                    SizeString += "\nLegal max key size = " + k.MaxSize + " bits";
+                }
+                ks = sa.LegalBlockSizes;
+                foreach (KeySizes k in ks)
+                {
+                    SizeString += "\n\nLegal min block size = " + k.MinSize + " bits";
+                    SizeString += "\nLegal max block size = " + k.MaxSize + " bits";
+                }
+
+                ICryptoTransform encryptor = sa.CreateEncryptor(sa.Key, sa.IV);
+                
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+        public static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV, CipherMode cm, PaddingMode pm, Algorithm a)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            
+            string plaintext = null;
+            
+            using (SymmetricAlgorithm sa = SymmetricAlgorithm.Create(a.ToString()))
+            {
+                sa.Key = Key;
+                sa.IV = IV;
+                sa.Mode = cm;
+                sa.Padding = pm;
+                
+                ICryptoTransform decryptor = sa.CreateDecryptor(sa.Key, sa.IV);
+                
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+}
---
+++ Implementare algoritmii criptare .net/Implementare algoritmii criptare .net/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Implementare_algoritmii_criptare.net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string normalText;
+        byte[] encrypted;
+        string roundtrip;
+        Algorithm encryptionAlgorithm;
+        CipherMode cm;
+        PaddingMode pm;
+        string SizeString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RulesUpdate();
+        }
+
+        /// <summary>
+        /// Reads text from file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ReadListBox.Items.Clear();
+            normalText = "";
+
+            try
+            {
+                TextReader tr = new StreamReader(@"../../TextFiles/NormalText.txt");
+                string line;
+                while ((line = tr.ReadLine()) != null)
+                {
+                    normalText += line + "\n";
+                    this.ReadListBox.Items.Add(normalText);
+                }
+            }
+            catch (Exception)
+            {
+                this.ReadListBox.Items.Add("Error in file reading!");
+            }
+        }
+
+        /// <summary>
+        /// Chose encryption algorithm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AlgorithmComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string algorithmComboBox = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+            encryptionAlgorithm = (Algorithm)Enum.Parse(typeof(Algorithm), algorithmComboBox);
+        }
+
+        /// <summary>
+        /// retruns the key from textbox
+        /// </summary>
+        /// <returns></returns>
+        private byte[] getKey()
+        {
+            byte[] key;
+            string[] bytes = this.KeyTextBox.Text.Split(' ');
+            key = new byte[bytes.Length];
+            for (int i = 0; i < key.Length; i++)
+            {
+                key[i] = byte.Parse(bytes[i]);
+            }
+            return key;
+        }
+
+        /// <summary>
+        /// returns block from textbox
+        /// </summary>
+        /// <returns></returns>
+        private byte[] getIV()
+        {
+            byte[] IV;
+            string[] bytes = this.IVTextBox.Text.Split(' ');
+            IV = new byte[bytes.Length];
+            for (int i = 0; i < IV.Length; i++)
+            {
+                IV[i] = byte.Parse(bytes[i]);
+            }
+            return IV;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.EncryptedListBox.Items.Clear();
+            try
+            {
+                ImplementEncryptionAlgorithm();
+                this.EncryptedListBox.Items.Add(System.Text.Encoding.ASCII.GetString(encrypted));
+            }
+            catch (Exception) { }
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DecryptedListBox.Items.Clear();
+            try
+            {
+                this.DecryptedListBox.Items.Add(roundtrip);
+            }
+            catch (Exception) { }
+        }
+
+        private void ImplementEncryptionAlgorithm()
+        {
+            try
+            {
+                byte[] key = getKey();
+                byte[] IV = getIV();
+                encrypted = EncryptionAlgorithm.EncryptStringToBytes(normalText, key, IV, cm, pm, encryptionAlgorithm,ref SizeString);
+                roundtrip = EncryptionAlgorithm.DecryptStringFromBytes(encrypted, key, IV, cm, pm, encryptionAlgorithm);
+                RulesUpdate();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+        }
+        
+        private void ModeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string ModeComboBox = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+            cm = (CipherMode)Enum.Parse(typeof(CipherMode), ModeComboBox);
+        }
+
+        private void PaddingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string PaddingComboBox = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+            pm = (PaddingMode)Enum.Parse(typeof(PaddingMode), PaddingComboBox);
+        }
+
+        /// <summary>
+        /// update rules listbox
+        /// </summary>
+        private void RulesUpdate()
+        {
+            this.RulesListBox.Items.Clear();
+            this.RulesListBox.Items.Add(SizeString);
+        }
+    }
+}
